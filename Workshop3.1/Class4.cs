c5b1ba81113a3a2bac62991e643d3ba025f1ab98
balaprasanna <balaprasannav2009@gmail.com>
2014-09-01 17:06:11 +0800

    bye

---
+++ Workshop3.1/Class4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3._1
+{
+    class Class4
+    {
+        int age;
+
+        public int Age
+        {
+            get {
+                return age;
+            }
+            set { 
+                age = value; 
+            }
+        }
+
+        static void Main(){
+            Class4 c4 = new Class4();
+            c4.Age = 17;
+            //new Class4().Age=6;
+    }
+
+    }
+}
---
+++ Workshop3.1/Class5.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3._1
+{
+    class Class5
+    {
+
+        static void Main()
+        {
+            Coin1 c = new Coin1();
+           // c.getFace();
+            Console.WriteLine(c.getFace());
+        }
+
+    }
+   class Coin1{
+
+        int face;
+       public int getFace()
+        {
+            return face;
+        }
+
+        //Constructor
+        //default instance constructor
+        public Coin1()
+        {
+            face = 1;
+            Console.WriteLine("default constructor coin  " + face);
+            // this is called everytime when an object is created,
+        }
+      
+
+    }
+}
