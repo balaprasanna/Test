847e245f3c797e58edca8ccaf7ca27d4eefa8645
balaprasanna <balaprasannav2009@gmail.com>
2014-09-01 14:11:43 +0800

    coin class

---
+++ Workshop3.1/Class2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3._1
+{
+    class Class2
+    {
+        static void Main(){
+
+       /* Random r = new Random();
+        int n = r.Next(2);
+        Console.WriteLine(n);*/
+
+// coin class
+        Coin c = new Coin();
+        c.flip();
+        int n =c.getFace();
+        Console.WriteLine(c.getFace());
+
+        }
+     }
+    class Coin
+    {
+        int face;
+       public void flip()
+        {
+            face = (new Random()).Next(2);
+        }
+        public int getFace()
+        {
+            return face;
+        }
+    }
+}
