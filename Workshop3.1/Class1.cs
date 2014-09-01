a2cfc675d4089d2afe35f2348d02bb16a645e238
balaprasanna <balaprasannav2009@gmail.com>
2014-09-01 13:47:52 +0800

    two main

---
+++ Workshop3.1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3._1
+{
+    // class to test two Main,
+    class Class1
+    {
+       static void Main()
+        {
+            Console.WriteLine("Main1 ");
+        }
+    }
+    class Classtemp
+    {
+        static void Main()
+        {
+            Console.WriteLine("Main 2");
+        }
+    }
+}
