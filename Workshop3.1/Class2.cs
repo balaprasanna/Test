aae3172aee54a5808bdde3ef4288f0500628f82f
balaprasanna <balaprasannav2009@gmail.com>
2014-09-01 16:20:31 +0800

    coin face

--- Workshop3.1/Class2.cs
+++ Workshop3.1/Class2.cs
@@ -8,27 +8,41 @@ namespace Workshop3._1
 {
     class Class2
     {
-        static void Main(){
+      
+        static void Main()
+        {
 
        /* Random r = new Random();
         int n = r.Next(2);
         Console.WriteLine(n);*/
 
 // coin class
+
         Coin c = new Coin();
+        Coin c1 = new Coin();
+        Coin c2 = new Coin();
         c.flip();
+        c1.flip();
+        c2.flip();
+
         int n =c.getFace();
-        Console.WriteLine(c.getFace());
+        Console.WriteLine(c.getFace() + "--" + c1.getFace() + "--" + c2.getFace());
 
         }
      }
-    class Coin
+  public class Coin
     {
-        int face;
+      const int HEAD = 0;
+      const int TAIL = 1;
+
+        int face = HEAD;
        public void flip()
         {
+            
             face = (new Random()).Next(2);
+            
         }
+      
         public int getFace()
         {
             return face;
