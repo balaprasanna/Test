cbd48d1c3eea0db4f0dbd6008ca0601ffac8eb22
balaprasanna <balaprasannav2009@gmail.com>
2014-09-06 00:18:57 +0800

    inheritance starting

---
+++ Workshop3.1/Class10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3._1
+{
+    class Class10
+    {
+        public static void Main()
+        {
+            Driver d = new Driver();
+         
+        }
+    }
+
+    class Car
+    {
+        Driver theowner;
+
+       public void setowner(Driver d)
+        {
+            theowner = d;
+        }
+
+    }
+    class Driver
+    {
+        Car mycar;
+        public void buy()
+        {
+            mycar = new Car();
+            mycar.setowner(this);
+        }
+    }
+}
