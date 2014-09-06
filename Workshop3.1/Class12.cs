f1bae4878acd39e80856e8c966faa15024d7067f
balaprasanna <balaprasannav2009@gmail.com>
2014-09-06 13:07:07 +0800

    fd

---
+++ Workshop3.1/Class12.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3._1
+{
+    class Class12
+    {
+        static void Main()
+        {
+     //       rectangle r = new rectangle(5, 4);
+            parallelogram p = new parallelogram(5,4,20);
+            Console.WriteLine("finally Area"+p.getArea()+"-perimeter"+p.getPerimeter());
+            parallelogram q = new parallelogram(10, 5, 34);
+            q.method1(q);
+        }
+    }
+    class rectangle
+    {
+      private int length;
+      private int width;
+      public rectangle(int len,int wid)
+      {
+          length = len;
+          width = wid;
+      }
+      public void setProperties(int l, int w){
+          length = l;
+          width = w;
+      }
+      public int getlength()
+      {
+          return length;
+      }
+      public int getwidth()
+      {
+          return width;
+      }
+      public int getArea()
+      {
+          return length * width;
+      }
+      public int getPerimeter()
+      {
+          return 2 * (length + width);
+      }
+
+    }
+
+    class parallelogram : rectangle
+    {
+        private int angle;
+        public parallelogram(int l,int w,int angle):base(l,w)
+        {
+            this.angle = angle;
+           // base.getArea();
+           // base.getPerimeter();
+            Console.WriteLine("Area" + "length "+this.getlength()+"width"+this.getwidth()+  getArea() + "-perimeter" + this.getPerimeter());
+            //method1(this);
+        }
+        public void method1(parallelogram pm)
+        {
+            pm.setProperties(2, 2);
+            Console.WriteLine("Area" + pm.getArea() + "-perimeter" + pm.getPerimeter());
+            
+        }
+    } 
+}
