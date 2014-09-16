ddcfbf793c113ae3b451d93a174768c211ee76ad
balaprasanna <balaprasannav2009@gmail.com>
2014-09-16 11:32:42 +0800

    test
    test

---
+++ Workshop3.1/Class13.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop3._1
+{
+    class Class13
+    {
+        static void Main()
+        {
+            cat mycat = new cat();
+            mycat.P();
+
+            Animal myanimal = new Animal();
+            myanimal.P();
+           
+            Animal newanimal= new cat();
+            newanimal.P();
+
+            cat newcat = (cat)newanimal;
+            newcat.P();
+
+            Animal a2;
+            a2 = mycat;
+            a2.P();
+          //  Console.WriteLine(newanimal.GetType());
+            
+        }
+    }
+    class Animal
+    {
+        public Animal()
+        {
+          //  Console.WriteLine("Animal constructor");
+        }
+        //public void P()
+        //{
+        //    Console.WriteLine("Animal class");
+        //}
+        public virtual void P()
+        {
+            Console.WriteLine("Cat class");
+        }
+    }
+    class cat : Animal
+    {
+        public cat() {
+          //  Console.WriteLine("Cat constructor");
+        }
+        //public new void P()
+        //{
+        //    Console.WriteLine("Cat class");
+        //}
+        public override void P()
+        {
+            Console.WriteLine("Cat class");
+        }
+    }
+}
